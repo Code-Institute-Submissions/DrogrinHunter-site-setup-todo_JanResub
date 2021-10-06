cc15ac268ed313f97ff292af8f803745fc5f8e2e
DrogrinHunter <lukewillmore67@gmail.com>
2021-10-06 20:15:29 +0000

    completed the function for the submit button when editing a task and it is now successfully updated the db

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def add_task():
 
         }
         mongo.db.tasks.insert_one(task)
-        flash("Task has been successfully added")
+        flash("Task Has Been Successfully Added")
         return redirect(url_for("get_tasks"))
 
     site_name = mongo.db.location.find().sort("site_name", 1)
@@ -125,8 +125,20 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
-    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "site_name": request.form.get("site_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Has Been Successfully Updated")
 
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     site_name = mongo.db.location.find().sort("site_name", 1)
     return render_template("edit_task.html", task=task, site_name=site_name)
 
